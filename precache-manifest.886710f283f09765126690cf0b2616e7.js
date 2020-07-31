c2ded46d4fd05fa01c0ed00d745f0910db77c0aa
Amit Meher <amitmeher567@gmail.com>
2020-08-01 02:04:01 +0530

    Updates

---
+++ precache-manifest.886710f283f09765126690cf0b2616e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40df4ae4cb591eee1556657be8c3306c",
+    "url": "/quick-quizzer/index.html"
+  },
+  {
+    "revision": "e68f519819d32ecbfabe",
+    "url": "/quick-quizzer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c02fcd9d448ada3026b5",
+    "url": "/quick-quizzer/static/js/2.872820c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quick-quizzer/static/js/2.872820c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68f519819d32ecbfabe",
+    "url": "/quick-quizzer/static/js/main.1aaa1afc.chunk.js"
+  },
+  {
+    "revision": "1aa0e6209c892e1aa62d",
+    "url": "/quick-quizzer/static/js/runtime-main.df52a123.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quick-quizzer/static/media/logo.5d5d9eef.svg"
+  }
+]);
